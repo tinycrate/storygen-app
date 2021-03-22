7233232478cc38d15fc2d9061ffc1f96137d1001
Ronald Chan <12611939+tinycrate@users.noreply.github.com>
2021-03-22 23:43:06 +0800

    Implemented API code. Not tested

--- server.py
+++ server.py
@@ -6,22 +6,120 @@ import threading
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
+model_manager = ModelManager()
 
-connected_clients = set()
-connected_clients_lock = threading.Lock()
+connected_clients = {}
+connected_clients_lock = threading.RLock()
 
-@socketio.on('new_job')
-def new_job():
-    pass
+class ClientInfo:
+    def __init__(self):
+        self.samplers = {}
+        self.lock = threading.RLock()
+
+class SamplerInfo:
+    def __init__(self, model_info, generator):
+        self.model_info = model_info
+        self.generator = generator
+        self.lock = threading.RLock()
+
+def get_client(sid):
+    client = connected_clients.get(sid, None)
+    if not client:
+        print(f"WARN: Unable to get client for sid={sid}.")
+    return client
+
+@socketio.on('start_new_sampler')
+def start_new_sampler(sampler_name, model_name, prefix, parameters):
+    """
+    Create a new sampler for a client. Sampling starts immediately.
+
+    Parameters:
+        sampler_name:
+            A name chosen by the client to identify the sampler.
+            Duplicated name will overwrite previous sampler.
+        model_name:
+            Name of model to be used
+        prefix:
+            The text before generation (Input prompt)
+        parameters:
+            A dictionary with additional parameters given to the sampler.
+    """
+    client = get_client(request.sid)
+    if client == None: return
+    with client.lock:
+        if sampler_name in client.samplers:
+            model_manager.free_model(client.samplers[sampler_name].model_info.name)
+        model_info = model_manager.acquire_model(model_name)
+        sampler = TextSampler(model_info, **parameters)
+        client.samplers[sampler_name] = SamplerInfo(model_info, sampler.sample_text(prefix))
+    sampler_serve_next(request.sid, sampler_name)
+
+def sampler_serve_next(sid, sampler_name):
+    client = get_client(sid)
+    if client == None: return
+    sampler = client.samplers.get(sampler_name, None)
+    if not sampler:
+        print(f"WARN: Unable to get sampler {sampler_name} from client {sid}.")
+    with sampler.lock:
+        sampled_text = next(sampler.generator, None)
+    if sampled_text != None:
+        socketio.emit('on_text_sampled',
+            (sampler_name, sampled_text,),
+            to=sid,
+            callback=lambda continue_sampling=False, *args: after_text_sampled(sid, sampler_name, continue_sampling)
+        )
+    else:
+        socketio.emit('on_text_sample_completed',
+            sampler_name,
+            to=sid
+        )
+        # Clean up sampler
+        with client.lock:
+            model_manager.free_model(sampler.model_info.name)
+            del client.samplers[sampler_name]
+
+def after_text_sampled(sid, sampler_name, continue_sampling):
+    """
+    Called when the client has acknoledged the sampled text
+    The client should return a value continue_sampling to indicate
+    whether the sampling should continue. If not, cleanups should be done
+    """
+    if continue_sampling:
+        sampler_serve_next(sid, sampler_name)
+    else:
+        # Clean up sampler
+        with client.lock:
+            model_manager.free_model(sampler.model_info.name)
+            del client.samplers[sampler_name]
 
 @socketio.on('connect')
 def on_connect():
+    print(f"Client connecting... sid={request.sid}")
     with connected_clients_lock:
-        connected_clients.add(request.sid)
+        if request.sid in connected_clients:
+            print(f"WARN: Client already existed for the same sid ({request.sid}). Might indicate serious memory leak. Attempting to cleanup.")
+            _free_client_resources(request.sid)
+        connected_clients[request.sid] = ClientInfo()
     print(f"Client connected! sid={request.sid}")
 
 @socketio.on('disconnect')
 def on_disconnect():
+    print(f"Client disconnecting... sid={request.sid}")
     with connected_clients_lock:
-        connected_clients.discard(request.sid)
+        if request.sid not in connected_clients:
+            print(f"WARN: Client to be disconnected does not exist. Resources cannot be cleaned up.")
+        else:
+            _free_client_resources(request.sid)
+            del connected_clients[request.sid]
     print(f"Client disconnected! sid={request.sid}")
+
+# This call is destructive, causes the client in an unusable state.
+# The client should be removed immediately after the call
+def _free_client_resources(sid):
+    client = connected_clients[sid]
+    with client.lock:
+        for sampler in client.samplers:
+            model_manager.free_model(sampler.model_info.name)
+        client.samplers.clear()
+        client.samplers = None # Causes further access to samplers crash early as it is not intended
+    model_manager.free_resources()

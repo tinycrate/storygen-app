345905664347a273e42cb6ae2d26e780c9fb2267
Ronald Chan <12611939+tinycrate@users.noreply.github.com>
2021-09-24 22:52:01 +0800

    Added README.md and frontend build for demo

---
+++ static/config.js
@@ -0,0 +1,12 @@
+// noinspection ES6ConvertVarToLetConst
+var ModelConfig = {
+    continuationModels : {
+        "GPT2-Small"   : "gpt2"
+    },
+    promptModels : {
+        "Fallback <Replace this model>"   : "gpt2"
+    }
+};
+
+// noinspection ES6ConvertVarToLetConst
+var APILocation = "127.0.0.1:5000";
